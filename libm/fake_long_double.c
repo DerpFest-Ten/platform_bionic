spsforks/android-bionic-libc

Use builtins for fma/fmax/fmin/nearbyint/rint/round/trunc on 32-bit ARM
All except fma/fmaf are only used for ARMv8 targets.

Change-Id: I6883743c896eeacbb53ee5e1a366e14d812b0297

libm/fake_long_double.c
@@ -54,3 +54,18 @@ long double exp2l(long double x) { return exp2(x); }
 long double logl(long double x) { return log(x); }
 long double log2l(long double x) { return log2(x); }
 #endif
+
+// builtins.cpp does not provide the long double wrappers for the
+// routines it implements.
+
+#if defined (__ARM_NEON__)
+long double fmal(long double a1, long double a2, long double a3) { return fma(a1, a2, a3); }
+#endif
+
+#if defined (__ARM_ARCH_8A__)
+long double ceill(long double x) { return ceil(x); }
+long double floorl(long double x) { return floor(x); }
+long double nearbyintl(long double x) { return nearbyint(x); }
+long double rintl(long double x) { return rint(x); }
+long double truncl(long double x) { return trunc(x); }
+#endif

DerpFest-Ten/platform_bionic

Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

libm/fake_long_double.c
@@ -54,3 +54,18 @@ long double exp2l(long double x) { return exp2(x); }
 long double logl(long double x) { return log(x); }
 long double log2l(long double x) { return log2(x); }
 #endif
+
+// builtins.cpp does not provide the long double wrappers for the
+// routines it implements.
+
+#if defined (__ARM_NEON__)
+long double fmal(long double a1, long double a2, long double a3) { return fma(a1, a2, a3); }
+#endif
+
+#if defined (__AARCH32__)
+long double ceill(long double x) { return ceil(x); }
+long double floorl(long double x) { return floor(x); }
+long double nearbyintl(long double x) { return nearbyint(x); }
+long double rintl(long double x) { return rint(x); }
+long double truncl(long double x) { return trunc(x); }
+#endif